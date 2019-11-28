cb4cfefb1210becfd89dffec73e3641bd4e5b66e
melk0sha <melk0shadev@gmail.com>
2019-11-28 12:43:46 +0300

    feat: final desktop slider

---
+++ script.js
@@ -0,0 +1,89 @@
+window.onload = function () {
+    const closeButtonProject1 = document.querySelector('#container-project1__button-back');  
+    const closeButtonProject2 = document.querySelector('#container-project2__button-back');
+    const resizeButtonProject1 = document.querySelector('#container-project1__button-size');
+    const resizeButtonProject2 = document.querySelector('#container-project2__button-size');
+  
+    // show iframe
+    const showFirstProject = () => {
+        this.event.preventDefault();
+        const wrapper = document.querySelector('.section-iframe__container-prj1');
+        wrapper.classList.toggle('container-hidden');
+    
+        if (window.innerWidth < 640) {
+            resizeButtonProject1.classList.add('hideButton');
+            resizeButtonProject2.classList.add('hideButton');
+        } else {
+            resizeButtonProject1.classList.remove('hideButton');
+            resizeButtonProject2.classList.remove('hideButton');
+            resizeButtonProject1.textContent = "Desktop";
+            resizeButtonProject2.textContent = "Desktop";
+        }
+    }
+  
+    const showSecondProject = () => {
+        this.event.preventDefault();
+        const wrapper = document.querySelector('.section-iframe__container-prj2');
+        wrapper.classList.toggle('container-hidden');
+    
+        if (window.innerWidth < 400) {
+            resizeButtonProject1.classList.add('hideButton');
+            resizeButtonProject2.classList.add('hideButton');
+        } else {
+            resizeButtonProject1.classList.remove('hideButton');
+            resizeButtonProject2.classList.remove('hideButton');
+            resizeButtonProject1.textContent = "Desktop";
+            resizeButtonProject2.textContent = "Desktop";
+        }
+    }
+  
+    // iframe buttons
+    const hideiFrame = () => {
+        event.target.parentElement.classList.add('container-hidden');
+        resizeButtonProject1.textContent = 'Desktop';
+        resizeButtonProject2.textContent = 'Desktop';
+    
+        const iFrame = event.target.parentNode.childNodes[1];
+        iFrame.classList.remove('iFrame-desktop');
+    };
+  
+    const showDesktop = () => {
+        const iFrame = event.target.parentNode.childNodes[1];
+    
+        const iFrameListArray = Array.from(iFrame.classList);
+        if (iFrameListArray.includes('iFrame-desktop')) {
+            event.target.textContent = 'Desktop';
+        } else {
+            event.target.textContent = 'Mobile';
+        }
+    
+        iFrame.classList.toggle('iFrame-desktop');
+    }
+  
+    closeButtonProject1.addEventListener('click', hideiFrame);
+    closeButtonProject2.addEventListener('click', hideiFrame);
+  
+    resizeButtonProject1.addEventListener('click', showDesktop);
+    resizeButtonProject2.addEventListener('click', showDesktop);
+  
+    // description 
+    const showDescription = () => {
+        if (window.innerWidth > 400) {
+            return;
+        }
+        event.target.parentNode.childNodes[3].classList.toggle('carousel-caption__description');
+    };
+  
+    const slider = this.document.querySelector('.carousel-inner');
+  
+    slider.addEventListener('click', showDescription);
+  
+    slider.addEventListener('click', () => {
+        if(event.target.id === 'project1') {
+            showFirstProject();
+        } else if(event.target.id === 'project2') {
+            showSecondProject();
+        }
+    });
+}
+  
